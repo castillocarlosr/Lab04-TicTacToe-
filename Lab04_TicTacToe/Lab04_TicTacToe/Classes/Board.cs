38b47992cea4fe2903dd24fa9298788da3546105
castillocarlosr <castillocarlosr@gmail.com>
2019-01-11 18:27:36 -0800

    added board game

--- Lab04_TicTacToe/Lab04_TicTacToe/Classes/Board.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Classes/Board.cs
@@ -7,8 +7,10 @@ namespace Lab04_TicTacToe.Classes
     class Board
     {
 		/// <summary>
-		/// Tic Tac Toe Gameboard states
+		/// A 2D array of the gameboard
 		/// </summary>
+        ///<param name="">Main</param>
+        /// <return>nothing</return>
 		public string[,] GameBoard = new string[,]
 		{
 			{"1", "2", "3"},
@@ -16,12 +18,23 @@ namespace Lab04_TicTacToe.Classes
 			{"7", "8", "9"},
 		};
 
-
+        /// <summary>
+        /// This is what displays to the console when called.
+        /// Basically have to double for loop to read and display the board game.
+        /// </summary>
+        /// <return>console</return>
 		public void DisplayBoard()
 		{
 
             //TODO: Output the board to the console
-            Console.WriteLine(GameBoard);
+            for (int i = 0; i < GameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < GameBoard.GetLength(1); j++)
+                {
+                    Console.Write(string.Format("{0}", GameBoard[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
             Console.ReadLine();
 		
 		}
--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -5,6 +5,12 @@ namespace Lab04_TicTacToe
 {
     class Program
     {
+        ///Carlos Castillo Jan. 11, 2019
+        /// <summary>
+        /// Should hopefully run and start the tic-tac-toe by calling something
+        /// </summary>
+        /// <param name="args">Main</param>
+        /// <return>console game</return>
         static void Main(string[] args)
         {
             Console.Clear();
@@ -15,7 +21,14 @@ namespace Lab04_TicTacToe
             Console.Write("Player 2 please type in your name: ");
             string inputNameTwo = Console.ReadLine();
             //Game TicTacToe = new Game(inputNameOne, inputNameTwo);
-            Console.WriteLine(GameBoard);
+            //Console.WriteLine(GameBoard);
+            DisplayBoard();
+
+        }
+
+        private static void DisplayBoard()
+        {
+            throw new NotImplementedException();
         }
     }
 }
